a9f4113c1354a24d271df5a02d411a2bb1abcb27
Xuntian Rao <xuntianrao@microsoft.com>
2021-11-11 16:09:53 +0800

    add Async Program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-//using HelloWorld;
+using HelloWorld.Test;
 
 namespace HelloWorld
 {
@@ -19,39 +18,13 @@ namespace HelloWorld
                 Console.WriteLine(grade);
             }
         }
-        static void Main(string[] args)
+
+        public void TableIdentifierTest()
         {
-            //var str = "Welcome to Microsoft!";
-
-            //Console.WriteLine("Please input your name:");
-            //var anything = Console.ReadLine();
-
-            //Console.WriteLine("Hello " + anything + ", " + str);
-            //Console.WriteLine("Hello {0}, {1}", anything, str);
-
-            //ArrayPrint();
-
-            var gg = new GenGuid();
-            gg.GenGuid2();
-            var gg1 = gg.SetGuid();
-            var gg2 = gg.SetGuidRandom();
-            var gg3 = gg.SetGuidRandom();
-            Console.WriteLine($"Guid1 is {gg1}.");
-            Console.WriteLine($"Guid2 is {gg2}.");
-            Console.WriteLine($"Guid3 is {gg3}.");
-
-            //var atest = new TestClass();
-            TestClass.GenObj();
-
-            //var enumtest = new EnumTest();
-            //enumtest.Main();
-
-            Symbol.wenhao();
-
             Console.Write("Input tablename: ");
             string tablename = Console.ReadLine();
 
-            while(!string.IsNullOrWhiteSpace(tablename))
+            while (!string.IsNullOrWhiteSpace(tablename))
             {
                 //var tablelist = TableIdentifierHelper.SplitIdentifier(tablename);
                 var tablelist = TableIdentifierHelper.SplitIdentifierForOracle(tablename);
@@ -72,6 +45,38 @@ namespace HelloWorld
                 Console.Write("Input tablename again: ");
                 tablename = Console.ReadLine();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            //var str = "Welcome to Microsoft!";
+
+            //Console.WriteLine("Please input your name:");
+            //var anything = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + anything + ", " + str);
+            //Console.WriteLine("Hello {0}, {1}", anything, str);
+
+            //ArrayPrint();
+
+            //var gg = new GenGuid();
+            //gg.GenGuid2();
+            //var gg1 = gg.SetGuid();
+            //var gg2 = gg.SetGuidRandom();
+            //var gg3 = gg.SetGuidRandom();
+            //Console.WriteLine($"Guid1 is {gg1}.");
+            //Console.WriteLine($"Guid2 is {gg2}.");
+            //Console.WriteLine($"Guid3 is {gg3}.");
+
+            //var atest = new TestClass();
+            //TestClass.GenObj();
+
+            //var enumtest = new EnumTest();
+            //enumtest.Main();
+
+            //Symbol.wenhao();
+
+            AsyncProgramTest.Excute();
 
 
             Console.ReadLine();
---
+++ HelloWorld/Test/AsyncProgramTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Test
+{
+    public class AsyncProgramTest
+    {
+        //public static int NoAsyncTest()
+        //{
+        //    return 1;
+        //}
+
+        //public static async Task<int> AsyncTest()
+        //{
+        //    return 1;
+        //}
+
+        public static async void Excute()
+        {
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Start Excute " + DateTime.Now);
+            await SingleAwait();
+            await SingleNoAwait();
+            //var waitTask = AsyncTestRun();
+            //waitTask.Start();
+            //int i = await waitTask;
+            //Console.WriteLine(Thread.CurrentThread.GetHashCode() + " i " + i);
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " End Excute " + DateTime.Now);
+        }
+
+        public static async Task<int> AsyncTest()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run1 " + DateTime.Now);
+                Thread.Sleep(1000);
+            }).GetAwaiter().GetResult();
+            return 1;
+        }
+
+        public static Task<int> AsyncTestRun()
+        {
+            Task<int> t = new Task<int>(() => {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run1 " + DateTime.Now);
+                Thread.Sleep(1000);
+                return 100;
+            });
+            return t;
+        }
+
+        public static async Task SingleAwait()
+        {
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " AwaitTest start " + DateTime.Now);
+            await Task.Run(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run1 " + DateTime.Now);
+                Thread.Sleep(1000);
+            });
+            await Task.Run(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run2 " + DateTime.Now);
+                Thread.Sleep(1000);
+            });
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " AwaitTest end " + DateTime.Now);
+            return;
+        }
+
+        public static async Task SingleNoAwait()
+        {
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " SingleNoAwait Start " + DateTime.Now);
+            Task.Run(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run1 " + DateTime.Now);
+                Thread.Sleep(1000);
+            }).GetAwaiter().GetResult();
+            Task.Run(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.GetHashCode() + " Run2 " + DateTime.Now);
+                Thread.Sleep(1000);
+            }).GetAwaiter().GetResult();
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + " SingleNoAwait End " + DateTime.Now);
+            return;
+        }
+
+    }
+}
